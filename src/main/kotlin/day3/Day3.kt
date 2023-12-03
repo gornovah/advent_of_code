d77cb440d75edb75e93b9b50db8d61058755e31a
Domingo Espinosa <gornovah@users.noreply.github.com>
2023-12-03 18:24:54 +0100

    Day 3 part 2 and refactor

--- src/main/kotlin/day3/Day3.kt
+++ src/main/kotlin/day3/Day3.kt
@@ -4,47 +4,69 @@ import load
 
 
 fun sumPartNumbers(schematic: List<String>): Int {
-    var total1 = 0
-    for (r in schematic.indices) {
-        var c = 0
-        while (c < schematic[r].length) {
-            if (schematic[r][c].isDigit()) {
-                var end = c
-                while (end < schematic[r].length && schematic[r][end].isDigit()) {
-                    end++
+    var total = 0
+    for (row in schematic.indices) {
+        var col = 0
+        while (col < schematic[row].length) {
+            if (schematic[row][col].isDigit()) {
+                val num = schematic[row].substring(col).takeWhile { it.isDigit() }.toInt()
+                if (isAdjacent(schematic, row, col)) {
+                    total += num
                 }
-                val num = schematic[r].substring(c, end).toInt()
-                if ((c until end).any { isAdjacent(schematic, r, it) }) {
-                    total1 += num
-                }
-                c = end
+                col += num.toString().length
             } else {
-                c++
+                col++
             }
         }
     }
-    return total1
+    return total
 }
 
-fun isAdjacent(grid: List<String>, r: Int, c: Int): Boolean {
-    val directions = listOf(
-        -1 to -1, -1 to 0, -1 to 1,
-        0 to -1,  0 to 1,
-        1 to -1,  1 to 0,  1 to 1
-    )
-
-    for ((dr, dc) in directions) {
-        val nr = r + dr
-        val nc = c + dc
-        if (nr in grid.indices && nc in grid[0].indices && grid[nr][nc] !in '0'..'9' && grid[nr][nc] != '.') {
-            return true
+fun isAdjacent(schematic: List<String>, row: Int, startCol: Int): Boolean {
+    val endCol = startCol + schematic[row].substring(startCol).takeWhile { it.isDigit() }.length
+    return (-1..1).any { dx ->
+        (-1..1).any { dy ->
+            (dx != 0 || dy != 0) && (row + dx in schematic.indices) && (startCol until endCol).any { col ->
+                col + dy in schematic[row + dx].indices && schematic[row + dx][col + dy] !in '0'..'9' && schematic[row + dx][col + dy] != '.'
+            }
         }
     }
-    return false
 }
 
+fun sumaPartNumbers2(schematic: List<String>): Int {
+    var total = 0
+    for ((row, line) in schematic.withIndex()) {
+        for ((col, char) in line.withIndex()) {
+            if (char == '*') {
+                val nums = getAdjacentNumbers(schematic, row, col)
+                if (nums.size == 2) {
+                    total += nums[0] * nums[1]
+                }
+            }
+        }
+    }
+    return total
+}
+
+fun getAdjacentNumbers(schematic: List<String>, row: Int, col: Int): List<Int> {
+    return (-1..1).flatMap { dx ->
+        (-1..1).mapNotNull { dy ->
+            if (dx != 0 || dy != 0) {
+                val newRow = row + dx
+                val newCol = col + dy
+                if (newRow in schematic.indices && newCol in schematic[newRow].indices && schematic[newRow][newCol].isDigit()) {
+                    val numStr = generateSequence(newCol) { if (it - 1 >= 0 && schematic[newRow][it - 1].isDigit()) it - 1 else null }
+                        .last()
+                        .let { startCol -> schematic[newRow].substring(startCol).takeWhile { it.isDigit() } }
+                    numStr.toIntOrNull()
+                } else null
+            } else null
+        }
+    }.distinct()
+}
 
 fun main() {
     val loadedData = load("/day-3.txt")
-    println("Solution to day 3, part 1 is '${sumPartNumbers(loadedData)}'")
+    //println("Solution to day 3, part 1 is '${sumPartNumbers(loadedData)}'")
+    println("Solution to day 3, part 2 is '${sumaPartNumbers2(loadedData)}'")
 }
